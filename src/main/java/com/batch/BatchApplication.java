3bbfc20f3bbd2a4d7b2d0fd560d19db7414dfe64
dlwnstn0524 <ljs052450@gmail.com>
2024-07-17 22:16:52 +0900

    feature: simpleJob success

--- src/main/java/com/batch/BatchApplication.java
+++ src/main/java/com/batch/BatchApplication.java
@@ -1,15 +1,29 @@
 package com.batch;
 
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableBatchProcessing
+
 @SpringBootApplication
-public class BatchApplication {
+public class BatchApplication implements CommandLineRunner {
+
+    @Autowired
+    private JobLauncher jobLauncher;
+
+    @Autowired
+    private Job hewwloWorldJob;
 
     public static void main(String[] args) {
         SpringApplication.run(BatchApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        jobLauncher.run(hewwloWorldJob, new JobParametersBuilder().toJobParameters());
+    }
 }
--- src/main/java/com/batch/job/MyBatchConfig.java
+++ src/main/java/com/batch/job/MyBatchConfig.java
@@ -1,40 +1,42 @@
 package com.batch.job;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.job.builder.JobBuilder;
-import org.springframework.batch.core.repository.JobRepository;
-import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.transaction.PlatformTransactionManager;
 
+@Slf4j
 @Configuration
 @EnableBatchProcessing
 public class MyBatchConfig {
 
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+
     @Bean
-    public Job job(JobRepository jobRepository, Step step1) {
-        return new JobBuilder("myJob", jobRepository)
-                .start(step1)
+    public Job simpleJob() {
+        return jobBuilderFactory.get("simpleJob")
+                .start(simpleStep())
                 .build();
     }
 
     @Bean
-    public Step step(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
-        return new StepBuilder("step1", jobRepository)
-                .tasklet(tasklet(), transactionManager)
+    public Step simpleStep() {
+        return stepBuilderFactory.get("simpleStep")
+                .tasklet((contribution, chunkContext) -> {
+                    log.debug(">>>>> THhis is simpleStep!!");
+                    return RepeatStatus.FINISHED;
+                })
                 .build();
     }
 
-    @Bean
-    public Tasklet tasklet() {
-        return (contribution, chunkContext) -> {
-            System.out.println("Hello World!");
-            return RepeatStatus.FINISHED;
-        };
-    }
 }
